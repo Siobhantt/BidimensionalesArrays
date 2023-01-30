eced787e7173c26e19d967eb6ba322ecf5900941
lcardozo <lcardozo@106-25.info2>
2023-01-30 09:15:01 +0100

    Debo terminar el 3

--- BidimensionesArrays/src/ejercicios/Ej3.java
+++ BidimensionesArrays/src/ejercicios/Ej3.java
@@ -13,6 +13,8 @@ public class Ej3 {
 	public static void main(String[] args) {
 		int min;
 		int max;
+		int suma;
+		double media;
 		int numUser;
 		int notas[][];
 		Scanner lee = new Scanner(System.in);
@@ -22,27 +24,44 @@ public class Ej3 {
 			System.out.println("Por favor introduzca las Notas del alumno " + (i + 1));
 // en este for solicito las notas
 			for (int j = 0; j <= notas.length; j++) {
-				numUser = lee.nextInt();//en caso de no ser validas imprimo un error
+				numUser = lee.nextInt();// en caso de no ser validas imprimo un error
 				if (numUser < 0 || numUser > 10) {
 					System.err.println("Las notas deben estar entre 0 y 10");
-					while (numUser<0||numUser>10) {//con este while vuelvo a solicitar las notas 
-						numUser=lee.nextInt();
-						notas[i][j]=numUser; //has que sean correctas y las guardo en la tabla
-						
+					while (numUser < 0 || numUser > 10) {// con este while vuelvo a solicitar las notas
+						numUser = lee.nextInt();
+						notas[i][j] = numUser; // has que sean correctas y las guardo en la tabla
+
 					}
 				} else {
-					notas[i][j] = numUser;//en caso de que sean correctas se guardan
+					notas[i][j] = numUser;// en caso de que sean correctas se guardan
 				}
 			} // for interno
 		} // for principal
 
-		for (int i = 0; i < notas.length; i++) {//aqui uso la formula que dio la profe para que salgan bien impresas
+		for (int i = 0; i < notas.length; i++) {// aqui uso la formula que dio la profe para que salgan bien impresas
 			System.out.print("Alumno " + (i + 1) + ": ");
 			for (int j = 0; j < notas[i].length; j++) {
 				System.out.print((notas[i][j]) + "\t");
 			}
 			System.out.println();
-		}//fin del for
+		} // fin del for
+		
+		for (int i=0; i<notas.length;i++){
+			min=10;
+			max=0;
+			suma=0;
+			for (int j=0;j<notas[i].length;j++) {
+			if (notas[i][j]>max) {
+				max=notas[i][j];
+			}
+			if(notas[i][j]<min) {
+				min=notas[i][j];
+			}
+			suma+=notas[i][j];
+			}//fin del for J
+			media =(double) suma/notas[i].length;
+			System.out.println("La nota maximo del alumno " +  );
+		}
 		
 		
 	}// fin del main
